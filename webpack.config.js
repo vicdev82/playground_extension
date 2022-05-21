0e8bbfee54c48d37e7f5250b766c393b1e07ac2a
Victor Grande <victormge1182@gmail.com>
2022-05-21 17:59:15 +0200

    [TASK] Add webpack mode development

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './packages/site_package/Resources/Private/Javascript/app.js',
   output: {
     filename: 'main.js',
-    path: path.resolve('packages/site_package/Resources/Private/Javascript/'),
+    path: path.resolve('packages/site_package/Resources/Public/Javascript/'),
   },
 };
